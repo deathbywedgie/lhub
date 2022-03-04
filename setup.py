348b48c9433976bcc409f98c905371ef2041e191
Chad Roberts <jcbroberts@gmail.com>
2022-03-03 17:45:59 -0800

    Initial setup and requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+setup(
+    name="lhub",
+    version=VERSION,
+    author="Chad Roberts",
+    author_email="chad@logichub.com",
+    description="LogicHub API Wrapper",
+    long_description="A Python package for interacting with LogicHub APIs",
+    packages=find_packages(),
+    install_requires=[
+        "requests"
+    ],
+    keywords=["python", "lhub", "LogicHub"],
+
+    # https://pypi.org/classifiers/
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Natural Language :: English",
+    ]
+)
