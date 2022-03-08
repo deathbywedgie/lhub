8d1729972b0ed0a6a40809e1cab744cb67014fc5
Chad Roberts <jcbroberts@gmail.com>
2022-03-07 19:01:34 -0800

    time zone translation isn't likely necessary and isn't working anyway, so removing the option for now

--- lhub/common/time.py
+++ lhub/common/time.py
@@ -2,13 +2,10 @@ import datetime
 
 import pytz
 
-DEFAULT_TIME_ZONE = "US/Pacific"
 DEFAULT_TIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 
-def epoch_time_to_str(time_sec, time_format=None, timezone=None):
-    timezone = timezone or DEFAULT_TIME_ZONE
+def epoch_time_to_str(time_sec, time_format=None):
     time_format = time_format if time_format else DEFAULT_TIME_FORMAT
     dt = datetime.datetime.fromtimestamp(time_sec)
-    dt = pytz.timezone(timezone).localize(dt)
     return dt.strftime(time_format)
